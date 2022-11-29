2d1a193ab18abe71ee9976a8ac84b0011d7c5755
unknown <thomas.n71@gmail.com>
2022-11-29 13:29:54 +0100

    WIP

--- enrich.py
+++ enrich.py
@@ -16,9 +16,73 @@ from docx.enum.text import WD_BREAK
 import typer
 import pynlpir
 from docx2pdf import convert
+import jieba
+import epitran
+
+language = "br"
+
+epi = epitran.Epitran('por-Latn')
+epi.transliterate("bom dia")
+
+phonetic_fs = {  "br": (lambda x: "todo")
+               , "zh": (lambda x: pinyin.get(x))}
+
+links_chinese = {"hyperlink_base": "https://www.mdbg.net/chinese/dictionary?page=worddict&wdrst=0&wdqb="
+                 , "google_translate": "https://translate.googleapis.com/translate_a/single?client=gtx&sl=zh-CN&tl=en&dt=t&q="
+                 }
+links_portuguese = {"hyperlink_base": "https://www.dicio.com.br/"
+                    , "google_translate": "https://translate.googleapis.com/translate_a/single?client=gtx&sl=pt&tl=en&dt=t&q="
+                    }
+
+links_all = {"zh": links_chinese, "br": links_portuguese}
+links=links_all[language]
+
+class Translator:
+
+    def __init__(self, language):
+        self.language = language
+
+    def get_phonetic(self, text):
+        ret = phonetic_fs[self.language](text)
+
+        return Character(ret, pinyin_size)
+
+    def translation(self, x):
+        # translate from chinese to english using a free translation service
+        url = links["google_translate"] + x
+        response = requests.get(url)
+        if response.status_code != 200:
+            raise Exception("ERROR: API request unsuccessful.")
+        ret = json.loads(response.content.decode("utf-8"))[0][0][0]
+        return Character(ret, translation_size)
 
 
-hyperlink_base = "https://www.mdbg.net/chinese/dictionary?page=worddict&wdrst=0&wdqb="
+    def tokenize(self, line, engine):
+
+        if self.language == "zh":
+            if engine == "jieba":
+
+                words = jieba.lcut(line)
+                tokenized = list(zip(words, len(words) * ["not_detected"]))
+                return tokenized
+
+            elif engine == "pynlpir":
+                pynlpir.open()
+                tokenized = pynlpir.segment(line)
+                pynlpir.close()
+                return tokenized
+
+        elif self.language == "br":
+            words = [" " + x for x in line.split(" ")]
+            tokenized = list(zip(words, len(words) * ["not_detected"]))
+            return tokenized
+
+        else:
+            raise Exception("ERROR: Language not supported.")
+
+translator = Translator(language)
+
+
 debug = False
 base_size = 40
 pinyin_size = base_size / 2
@@ -100,19 +164,17 @@ def word_for_word(x, notranslate=False):
         return Character("", word_for_word_size)
 
     try:
-        # ret = c.unihan.lookup_char(x).first().kDefinition.split(",")[0][:10]
-
-        # dont translate some words
-
-        # get first few full words that are shorter than 10 together
         if x not in dictionary:
-            trans = translation(x).text
+            trans = translator.translation(x).text
         elif dictionary[x][:10] == "variant of":
-            trans = translation(x).text
+            trans = translator.translation(x).text
         elif dictionary[x][:7] == "surname":
-            trans = translation(x).text
+            trans = translator.translation(x).text
         else:
             trans = dictionary[x]
+
+        # remove everything between parentheses
+        trans = re.sub(r"\(.*\)", "", trans)
 
         if trans[:6] == "to be ":
             trans = trans[6:]
@@ -139,21 +201,6 @@ def word_for_word(x, notranslate=False):
 
     return Character(ret, word_for_word_size)
 
-
-def translation(x):
-    # translate from chinese to english using a free translation service
-    url = "https://translate.googleapis.com/translate_a/single?client=gtx&sl=zh-CN&tl=en&dt=t&q=" + x
-    response = requests.get(url)
-    if response.status_code != 200:
-        raise Exception("ERROR: API request unsuccessful.")
-    ret = json.loads(response.content.decode("utf-8"))[0][0][0]
-    return Character(ret, translation_size)
-
-
-def get_pinyin(x):
-    # get pinyin from text x
-    ret = pinyin.get(x)
-    return Character(ret, pinyin_size)
 
 
 def pad(x, length):
@@ -207,12 +254,14 @@ def add_hyperlink(paragraph, text, url, color, font_name, size):
     return hyperlink
 
 
-def enrich_txt(input_path:str, output_path:str, use_notranslate_file:bool=True) -> None:
+
+def enrich_txt(input_path:str, output_path:str, use_notranslate_file:bool=True, engine="pynlpir") -> None:
     """
     Enriches a txt file with pinyin, translation and word for word translation.
     :param input_path: path to input txt file
     :param output_path: path to output docx file
     :param use_notranslate_file: if True, uses notranslate.txt to not translate certain words
+    :param engine: engine to use for word tokenization, either "pynlpir" or "jieba"
     :return: None
 
     """
@@ -245,7 +294,7 @@ def enrich_txt(input_path:str, output_path:str, use_notranslate_file:bool=True) 
             run.font.name = font_name
             # add hyperlink
             if add_dict_link:
-                url = "https://www.mdbg.net/chinese/dictionary?page=worddict&wdrst=0&wdqb=" + text_list[i].text
+                url = links["hyperlink_base"] + text_list[i].text
                 add_hyperlink(p, text_list[i].text, url, text_list[i].color, font_name, Pt(text_list[i].size))
             else:
                 run = p.add_run(text_list[i].text)
@@ -266,22 +315,19 @@ def enrich_txt(input_path:str, output_path:str, use_notranslate_file:bool=True) 
     for line in tqdm(data):
         line = preprocess(line)
         p = document.add_paragraph("")
-        run = p.add_run(translation(line).text + "\n")
+        run = p.add_run(translator.translation(line).text + "\n")
         run.font.size = Pt(translation_size)
         run.font.name = "Arial"
         wfw_string, pad_wfw = [], []
         py_string, pad_py = [], []
         char_string, pad_char = [], []
 
-
-        pynlpir.open()
-
-        tokenized = pynlpir.segment(line)
+        tokenized = translator.tokenize(line, engine)
         sizes = []
         for token, word_type in tokenized:
 
             wfw = word_for_word(token, notranslate)
-            py = get_pinyin(token)
+            py = translator.get_phonetic(token)
             token = Character(token, base_size)
             token.set_color(color_coding.get(word_type, RGBColor(0, 0, 0)))
             size = max(wfw.len(), py.len(), token.len())
@@ -328,5 +374,5 @@ def enrich_txt(input_path:str, output_path:str, use_notranslate_file:bool=True) 
 
 
 if __name__ == "__main__":
-    typer.run(enrich_txt)
-    #enrich_txt("input.txt", "output.docx", use_notranslate_file=True)
+    #typer.run(enrich_txt)
+    enrich_txt("input.txt", "output.docx", use_notranslate_file=True)
